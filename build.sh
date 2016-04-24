425fb962ca0ed34f61653983a17da8e092ccef6e
Federico Brigante <bfred-it@users.noreply.github.com>
2016-04-24 10:07:25 +0700

    Updated rollup-babel-lib-bundler
    It allows custom names without renaming

--- build.sh
+++ build.sh
@@ -9,29 +9,31 @@
 output_filename=$1 # used for: module-name.js
 global_var=$2 # used for: window.moduleName
 
+cjs_name=.common-js
+es6_name=.es-modules
+iife_name=.browser
+
 if [[ $global_var ]]; then
 	# with IIFE
 	rollup-babel-lib-bundler \
 		--lib-name "$output_filename" \
-		--format cjs,es6,iife \
 		--module-name "$global_var" \
+		--format cjs,es6,iife \
+		--postfix cjs:"$cjs_name",es6:"$es6_name" \
 		--dest dist index.js
 
 	# only mangle names inside the IIFE
 	uglifyjs \
 		--mangle -- \
-		dist/"$output_filename".iife.js > dist/"$output_filename".browser.js
+		dist/"$output_filename".iife.js > dist/"$output_filename""$iife_name".js
 
-	## delete the non-minified version
+	# delete the non-minified version
 	rm dist/"$output_filename".iife.js
 else
 	# no IIFE
 	rollup-babel-lib-bundler \
 		--lib-name "$output_filename" \
 		--format cjs,es6 \
+		--postfix cjs:"$cjs_name",es6:"$es6_name" \
 		--dest dist index.js
 fi
-
-# clearer naming
-mv dist/"$output_filename".js dist/"$output_filename".common-js.js
-mv dist/"$output_filename".es2015.js dist/"$output_filename".es-modules.js
