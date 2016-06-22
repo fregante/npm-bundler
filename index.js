1c58bed3bb13faad1915b3648daace8399fce289
Federico Brigante <bfred-it@users.noreply.github.com>
2016-06-22 16:45:46 +0700

    Add npm text banner to builds

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 'use strict';
+const path = require('path');
 const rollup = require('rollup').rollup;
 const buble = require('rollup-plugin-buble');
 const uglify = require('rollup-plugin-uglify');
 const requireExternals = require('rollup-plugin-node-resolve');
 const filesize = require('rollup-plugin-filesize');
+
+const packageInfo = require(path.resolve('./package.json'));
+const banner = `/*! npm.im/${packageInfo.name} */`;
 
 const outputFilename = process.argv[2];
 let globalVarName = process.argv[3];
@@ -13,7 +17,8 @@ let cjsName = 'common-js';
 let esName = 'es-modules';
 let iifeName = 'browser';
 
-if (globalVarName === '--byte-count') {
+const isByteCountingOnly = globalVarName === '--byte-count';
+if (isByteCountingOnly) {
 	globalVarName = 'bytes';
 	iifeName = 'byte-count';
 }
@@ -33,11 +38,13 @@ rollup({
 }).then(bundle => Promise.all([
 	bundle.write({
 		format: 'cjs',
-		dest: cjsName
+		dest: cjsName,
+		banner
 	}),
 	bundle.write({
 		format: 'es',
-		dest: esName
+		dest: esName,
+		banner
 	})
 ])).catch(err => console.log(err));
 if (globalVarName) {
@@ -46,18 +53,27 @@ if (globalVarName) {
 		entry: 'index.js',
 		plugins: [
 			buble(),
-			uglify(),
 			requireExternals({
 				browser: true,
 				jsnext: true
 			}),
+			uglify(isByteCountingOnly ? {
+				output: {
+					comments: (node, comment) => {
+						if (comment.type === 'comment2') {
+							return comment.value[0] === '!';
+						}
+					}
+				}
+			} : {}),
 			filesize()
 		]
 	}).then(bundle =>
 		bundle.write({
 			format: 'iife',
 			moduleName: globalVarName,
-			dest: iifeName
+			dest: iifeName,
+			banner
 		})
 	).catch(err => console.log(err));
 }
